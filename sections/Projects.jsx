ed51cfaf038543c490519a5d93b6ae291d212023
unknown <dengzecheng@hotmail.com>
2023-05-13 11:16:50 -0700

    add project link in nav bar

--- sections/Projects.jsx
+++ sections/Projects.jsx
@@ -7,17 +7,20 @@ import ProjectModal from "@/components/ProjectModal";
 const Projects = () => {
     const [selected, setSelected] = useState(null);
     return (
-        <div className='max-w-[1240px] mx-auto min-h-screen px-2 py-16'>
-            <h1 className="text-regal-yellow text-5xl m-10 font-AbrilFatface">
-                Projects
-            </h1>
-                <div className='grid md:grid-cols-4 gap-8 justify-center align-middle'>
+        <div>
+            <a id ="projects" className="anchor"></a>
+            <div className='max-w-[1240px] mx-auto min-h-screen px-2 py-16'>
+                <h1 className="text-regal-yellow text-5xl m-10 font-AbrilFatface">
+                    Projects
+                </h1>
+                    <div className='grid md:grid-cols-4 gap-8 justify-center align-middle'>
 
-                    {projectList.map((item) => (
-                        <ProjectCard key={item.id} setSelected={setSelected} project={item} />
-                    ))}
-                </div>
-            {/*<ProjectModal selected={selected} setSelected={setSelected}/>*/}
+                        {projectList.map((item) => (
+                            <ProjectCard key={item.id} setSelected={setSelected} project={item} />
+                        ))}
+                    </div>
+                {/*<ProjectModal selected={selected} setSelected={setSelected}/>*/}
+            </div>
         </div>
     );
 };
